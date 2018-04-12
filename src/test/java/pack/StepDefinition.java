e9bd2c132c10ffc4f810aa29ccb78dcdbae47e93
SuperRio <mario.nady07@gmail.com>
2018-04-12 18:11:44 +0200

    some enhancement in code

--- src/test/java/pack/StepDefinition.java
+++ src/test/java/pack/StepDefinition.java
@@ -183,6 +183,7 @@ public class StepDefinition {
 	@Then("^I shall view  Confirmation page$")
 	public void i_shall_view_Confirmation_page() throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
+		System.out.println(" End of text :D ");
 		throw new PendingException();
 	}
 
